2c101f12c78353418ebed84778d3e7b8024cc7b3
Takahiro Ishii <takahiroishii@Takahiros-MacBook-Pro.local>
2019-05-29 23:31:40 -0500

    texas highways

--- js/main.js
+++ js/main.js
@@ -105,23 +105,10 @@ jQuery(document).ready(function($){
   	}
   })
 
-});
+  // slideshow
+  $('.slider').bxSlider({
+      auto: true,
+      pause: 60000,
+  });
 
-// // slideshow
-// var slideIndex = 1;
-// showDivs(slideIndex);
-//
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-//
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[slideIndex-1].style.display = "block";
-// }
+});
